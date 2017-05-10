3349d02b973c5dbc1844bc044a5d3f599d3aba57
Ryan-Murphy <mammoth@hotmail.ca>
2017-05-10 14:54:24 -0300

    Added a new text box and button to copy text field down into already present text box.

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,6 +2,9 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Locate all of our views
+        final Button button = (Button) findViewById(R.id.button);
+        final TextView textViewReceive = (TextView) findViewById(R.id.helloText);
+        final TextView textViewInput = (TextView) findViewById(R.id.editText);
+
+        // Add event listener for button click
+        // Template from: https://developer.android.com/reference/android/widget/Button.html
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                if(textViewInput.length() != 0){
+                    textViewReceive.setText(textViewInput.getText());
+                }
+            }
+        });
     }
 }
